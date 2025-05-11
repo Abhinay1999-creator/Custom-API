3ec0a6e40dbfe6ee339d10a5b35686acdad0965c
Abhinay1999-creator <bhosalesarjerao1@gmail.com>
2025-05-11 23:10:43 +0530

    Added functionaliyt for fetching url

--- src/CustomApi.js
+++ src/CustomApi.js
@@ -4,7 +4,15 @@ const CustomApi = ({url}) => {
 
     const [data, setData] = useState(null);
 
+    const getData = async () => {
+          const response = await fetch(url)
+          const data = await response.json()
+          setData(data)
+      }
   
+      useEffect(()=>{
+          getData()
+      },[url])
 
 
     return (
